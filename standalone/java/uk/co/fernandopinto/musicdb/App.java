bfa8bc470ed6d389269dce81ce1563eac72c43d0
Fernando Pinto <32436981+FernandoGPinto@users.noreply.github.com>
2017-12-10 19:50:48 +0000

    Update App.java

--- standalone/java/uk/co/fernandopinto/musicdb/App.java
+++ standalone/java/uk/co/fernandopinto/musicdb/App.java
@@ -3,38 +3,12 @@ package test;
 /**
  * Created by Fernando on 08/10/2017.
  */
-import java.io.IOException;
-import java.io.Reader;
-import java.io.StringReader;
-import java.util.Date;
-import java.util.List;
-
 import javafx.application.Application;
-import javafx.beans.property.StringProperty;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVRecord;
-import org.hibernate.Session;
-
-import org.hibernate.criterion.Projections;
-import org.hibernate.query.Query;
-import test.entities.Order;
-import test.entities.OrderLine;
-import test.entities.Product;
-import test.entities.ProductDTO;
 import test.util.JPAUtil;
-
-import javax.persistence.EntityManager;
-
-import static org.hibernate.testing.transaction.TransactionUtil.doInJPA;
 
 public class App extends Application {
 
