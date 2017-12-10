2fb3bc7871f63511211855ed095ebe4ef7351cad
Fernando Pinto <32436981+FernandoGPinto@users.noreply.github.com>
2017-12-10 19:10:47 +0000

    Update App.java

--- standalone/java/uk/co/fernandopinto/musicdb/App.java
+++ standalone/java/uk/co/fernandopinto/musicdb/App.java
@@ -45,128 +45,6 @@ public class App extends Application {
     public static void main(String[] args) {
 
         launch(args);
-
-        /*Reader in = new StringReader("a,b,c");
-        try {
-            for (CSVRecord record : CSVFormat.DEFAULT.parse(in)) {
-                for (String field : record) {
-                    System.out.print("\"" + field + "\", ");
-                }
-                System.out.println();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }*/
-
-
-        /*System.out.println("Hibernate many to many (Annotation)");
-
-        EntityManager em = JPAUtil.getEntityManager();
-        em.getTransaction().begin();
-
-        Product product = new Product("cd", "image", 4556, 50, 15);
-        em.persist(product);
-        product = new Product("vinyl", "image", 5556, 20, 25);
-        em.persist(product);
-
-        Order entities = new Order("John", "23 Some Street");
-        em.persist(entities);
-
-        Session session = em.unwrap( Session.class );
-        Product prod = session
-                .bySimpleNaturalId(Product.class)
-                .load( 4556 );
-
-        OrderLine orderLine = new OrderLine(4, entities, prod);
-        em.persist(orderLine);
-
-        em.getTransaction().commit();
-
-        String hql = "FROM Order";
-        Query query = (Query) em.createQuery(hql);
-        List<Order> results = query.list();
-        for(Order ord:results){
-            for(OrderLine ordLine:ord.getLines()) {
-                System.out.println("prod name: " + ordLine.getProduct().getName());
-            }
-        }
-
-        String hql1 = "FROM Product";
-        Query query1 = (Query) em.createQuery(hql1);
-        List<Product> results1 = query1.list();
-        for(Product prod:results1){
-            for(OrderLine ordLine:prod.getLines()) {
-                System.out.println("ord name: " + ordLine.getOrder().getCustomerName());
-            }
-        }
-
-        Session session = em.unwrap(Session.class);
-        Product prod = session.bySimpleNaturalId(Product.class).load(4556);
-        System.out.println("prod price: " + prod.getPrice());
-
-        List<OrderLine> res = em.createQuery("select s " +
-                "from OrderLine s " +
-                "join fetch s.entities", OrderLine.class)
-                .getResultList();
-
-        for(OrderLine ordLine :res){
-                System.out.println("prod qty: " + ordLine.getQuantity());
-                System.out.println("prod price: " + ordLine.getProduct().getPrice());
-                System.out.println("ord address: " + ordLine.getOrder().getCustomerAddress());
-        }
-
-        query = session.createNamedQuery("getByOrderId").setParameter("orderId", 1);
-        List<Order> o = query.list();
-        for(Order ord:o) {
-            System.out.println("query ord name: " + ord.getCustomerName());
-        }
-
-        query = session.createNamedQuery("getByCustomerName").setParameter("customerName", "John");
-        o = query.list();
-        for(Order ord:o) {
-            System.out.println("query ord date: " + ord.getDate());
-        }
-
-        SimpleDateFormat ft = new SimpleDateFormat("yyyy-MM-dd");
-        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-
-        Date begin = null;
-        Date end = null;
-
-        try {
-            begin = ft.parse("2017-10-12");
-            end = ft.parse("2017-10-14");
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        query = session.createNamedQuery("getByPeriod").setParameter("begin", begin).setParameter("end", end);
-        o = query.list();
-        for(Order ord:o) {
-            System.out.println("query ord id: " + ord.getOrderId());
-        }
-
-
-        em.close();
-        JPAUtil.close();
-
-        entities entities = new entities();
-
-        entities.setStockCode("1939");
-        entities.setStockName("VINYL");
-
-        StockDetail stockDetail = new StockDetail();
-        stockDetail.setCompName("Megamusica");
-        stockDetail.setCompDesc("music");
-        stockDetail.setRemark("vinci vinci");
-        stockDetail.setListedDate(new Date());
-
-        entities.setStockDetail(stockDetail);
-        stockDetail.setOrder(entities);
-
-        session.save(entities);
-        session.getTransaction().commit();
-
-        System.out.println("Done");*/
     }
 
     @Override
@@ -179,8 +57,6 @@ public class App extends Application {
         stage.setScene(scene);
         stage.setResizable(true);
         stage.show();
-
-        //
     }
 
     @Override
